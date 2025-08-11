af59f2e78c0da4c2ade9340aa7336be56f53acc1
Patryk <patrykmigala0@gmail.com>
2025-08-11 18:01:01 +0200

    add basic structure of rotues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import AppLayout from "./AppLayout"
+import Home from "./pages/Home"
 
 function App() {
     return (
-        <div>
-            app
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AppLayout />}>
+                    <Route index element={<Navigate replace to='social-network-project/home' />} />
+                    <Route path="social-network-project/home" element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
